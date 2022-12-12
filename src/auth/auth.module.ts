b7eeb5fe7ebaa3ddac1a6b1a64ee52d8929f91d3
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2022-12-12 16:41:28 -0300

    :wrench: Add passport imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './util/local.strategy';
 
 @Module({
-  providers: [AuthService],
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
